c67002a27e22af69c9117c290ca3c0df4dbd2d7d
Janusred <horaciok300@hotmail.com>
2022-10-22 01:28:15 -0500

    Network Fallback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-
+importScripts('js/sw-utils.js');
 const STATIC_CACHE    = 'static-v1';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
@@ -46,4 +46,17 @@ self.addEventListener('activate', e => {
     });
     });
     e.waitUntil( respuesta );
+});
+
+self.addEventListener( 'fetch', e => {
+    const respuesta = caches.match( e.request ).then( res => {
+        if ( res ) {
+            return res;
+        } else {
+            return fetch( e.request ).then( newRes => {
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+            });
+        }
+    });
+    e.respondWith( respuesta );
 });
